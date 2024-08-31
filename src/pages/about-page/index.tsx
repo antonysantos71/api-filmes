27e962484e97f80c4143118c0dfcab50b4dfa8bd
antonysantos71 <ribeiroantony02.56@gmail.com>
2024-08-31 16:43:31 -0300

    style: bg-zinc-800

--- src/pages/about-page/index.tsx
+++ src/pages/about-page/index.tsx
@@ -5,7 +5,7 @@ export function AboutPage() {
   return (
     <>
     <NavBar/>
-      <div className="max-w-4xl mx-auto p-6 bg-white dark:bg-gray-900 shadow-md rounded-lg m-5">
+      <div className="max-w-4xl mx-auto p-6 bg-zinc-800 shadow-md rounded-lg m-5">
       <h1 className="text-3xl font-bold text-gray-900 dark:text-white mb-4">Sobre Nós</h1>
       <p className="text-gray-700 dark:text-gray-300 mb-4">
         Bem-vindo à nossa aplicação de filmes! CineStream, Somos apaixonados por cinema e criamos esta aplicação para ajudar você a explorar e descobrir filmes incríveis. Aqui você pode encontrar uma vasta coleção de filmes, obter detalhes sobre cada um deles e muito mais.
